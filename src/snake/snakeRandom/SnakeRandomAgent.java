abcf7a780f10b895a228a8f6c47029975e2f0e28
goncalo123mota <goncalo.sousa.mota@gmail.com>
2018-05-07 19:52:47 +0100

    SnakeRandom

--- src/snake/snakeRandom/SnakeRandomAgent.java
+++ src/snake/snakeRandom/SnakeRandomAgent.java
@@ -3,9 +3,14 @@ package snake.snakeRandom;
 import snake.*;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class SnakeRandomAgent extends SnakeAgent {
     // TODO
+
+    private ArrayList<Character> pickRandomPosition = new ArrayList<>();
+    private Random random = new Random();
 
     public SnakeRandomAgent(Cell cell, Color color) {
         super(cell, color);
@@ -13,7 +18,44 @@ public class SnakeRandomAgent extends SnakeAgent {
 
     @Override
     protected Action decide(Perception perception) {
-        // TODO
+
+
+
+        Cell n = perception.getN();
+        Cell s = perception.getS();
+        Cell e = perception.getE();
+        Cell w = perception.getW();
+
+        if(s != null &&  !s.hasAgent() && !s.hasTail()) {
+            pickRandomPosition.add('s');
+        }
+        if(n != null && !n.hasAgent() && !n.hasTail() ) {
+            pickRandomPosition.add('n');
+        }
+        if( e != null && !e.hasAgent() && !e.hasTail() ) {
+            pickRandomPosition.add('e');
+        }
+        if(w != null && !w.hasAgent() && !w.hasTail() ) {
+            pickRandomPosition.add('w');
+        }
+
+
+        char randomDirection = pickRandomPosition.get(random.nextInt(pickRandomPosition.size()));
+        pickRandomPosition.clear();
+
+        if (randomDirection == 'n'){
+            return Action.NORTH;
+
+        }else if (randomDirection == 's'){
+            return Action.SOUTH;
+
+        }else if(randomDirection == 'e'){
+            return Action.EAST;
+
+        }else if(randomDirection == 'w'){
+            return Action.WEST;
+        }
+
         return null;
     }
 }
